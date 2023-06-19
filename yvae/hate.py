307f89d48e75760af5cac5cb809853e431da78b4
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-06-19 01:59:12 -0400

    deleted junk

--- yvae/hate.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("../data_utils")
-from yvae_data_helper import *
-
-dataset_names=["jlbaker361/flickr_humans_10k","jlbaker361/anime_faces_10k"]
-test_dataset=yvae_get_labeled_dataset_test(4, dataset_names=dataset_names,image_dim=64)
-
-for (img,labels) in test_dataset:
-    print(labels)
