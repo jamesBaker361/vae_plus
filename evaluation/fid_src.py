1158f5946539440c881abf12251b3b5fb9df987d
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2023-06-19 21:23:47 -0400

    broke into two methods

--- evaluation/fid_src.py
+++ evaluation/fid_src.py
@@ -3,20 +3,18 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3
 import numpy as np
 from scipy.linalg import sqrtm
 
-
-def calculate_fid(input_shape,images1,images2):
-    # calculate activations
+def calculate_mu_sig(input_shape,images):
     model = InceptionV3(include_top=False, pooling='avg', input_shape=input_shape)
-    images1=tf.keras.applications.inception_v3.preprocess_input(images1, data_format=None)
-    images2=tf.keras.applications.inception_v3.preprocess_input(images2, data_format=None)
+    images=tf.keras.applications.inception_v3.preprocess_input(images, data_format=None)
     resize_shape=(input_shape[0], input_shape[1])
-    images1=tf.image.resize(images1, resize_shape)
-    images2=tf.image.resize(images2, resize_shape)
-    act1 = model.predict(images1)
-    act2 = model.predict(images2)
+    images=tf.image.resize(images, resize_shape)
+    act = model.predict(images)
     # calculate mean and covariance statistics
-    mu1, sigma1 = act1.mean(axis=0), np.cov(act1, rowvar=False)
-    mu2, sigma2 = act2.mean(axis=0), np.cov(act2, rowvar=False)
+    mu, sigma = act.mean(axis=0), np.cov(act, rowvar=False)
+    return mu, sigma
+
+
+def calculate_fid(input_shape, mu1, sigma1,mu2, sigma2):
     # calculate sum squared difference between means
     ssdiff = np.sum((mu1 - mu2)**2.0)
     # calculate sqrt of product between cov
