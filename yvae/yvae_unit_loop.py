9b73641bdb25515af500e84a79312b5c507fb469
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-06-30 15:55:21 -0400

    got rid of mirrored stuff

--- yvae/yvae_unit_loop.py
+++ yvae/yvae_unit_loop.py
@@ -56,64 +56,26 @@ def objective_unit(trial,args):
     mirrored_strategy = tf.distribute.MirroredStrategy(logical_gpus)
     #mirrored_strategy = tf.distribute.MirroredStrategy()
     start=time.time()
-    if args.disable_strategy==False:
-        with mirrored_strategy.scope():
-            print(mirrored_strategy)
-            print("begin mirrored stuff")
-
-            data_start=time.time()
-            dataset_dict=yvae_get_dataset_train(batch_size=args.batch_size, dataset_names=args.dataset_names, image_dim=args.image_dim,mirrored_strategy=mirrored_strategy)
-            test_dataset_dict=yvae_get_dataset_test(batch_size=args.batch_size, dataset_names=args.dataset_names, image_dim=args.image_dim, mirrored_strategy=mirrored_strategy)
-            data_end=time.time()
-            print("seeting up data took {} seconds ".format(data_end-data_start))
-            
-            optimizer_start=time.time()
-            optimizer=keras.optimizers.Adam(learning_rate=args.init_lr)
-            unfrozen_optimizer=keras.optimizers.Adam(learning_rate=0.00001)
-            print("optimizers took {} seconds".format(time.time()-optimizer_start))
-            #optimizer=tf.keras.mixed_precision.LossScaleOptimizer(optimizer)
-            process = psutil.Process(os.getpid())
-            pct=process.memory_percent()
-            print('mem pct',pct)
-            time.sleep(5)
-            gc.collect()
-            if args.load:
-                print("loading from saved model")
-                shared_partial=tf.keras.models.load_model(save_model_folder+SHARED_ENCODER_NAME)
-                decoders=[tf.keras.models.load_model(save_model_folder+DECODER_NAME.format(i)) for i in range(n_classes)]
-                partials=[tf.keras.models.load_model(save_model_folder+UNSHARED_PARTIAL_ENCODER_NAME.format(i)) for i in range(n_classes)]
-                unit_list=load_unit_list(shared_partial, decoders, partials)
-
-                with open(save_model_folder+"/meta_data.json","r") as src_file:
-                    start_epoch=json.load(src_file)["epoch"]
-
-                print("successfully loaded from {} at epoch {}".format(save_model_folder, start_epoch))
-
-            else:
-                print("not loading from saved")
-                encoder_start=time.time()
-                print('encoder args',input_shape,args.latent_dim, args.use_residual)
-                if len(args.pretrained_creativity_path)==0:
-                    encoder=get_encoder(input_shape,args.latent_dim, use_residual=args.use_residual, use_bn=args.use_bn,use_gn=args.use_gn)
-                else:
-                    encoder=tf.keras.models.load_model(args.pretrained_creativity_path)
-                    print('loaded from creativity!')
-                encoder_end=time.time()
-                print('getting encoder took {} time'.format(encoder_end-encoder_start))
-                mid_name=ENCODER_CONV_NAME.format(2)
-                unit_list=get_unit_list(input_shape,args.latent_dim,n_classes,encoder,mid_name=mid_name, use_residual=args.use_residual,use_bn=args.use_bn)
-                print("unit_list time took {}".format(time.time()-encoder_end))
-
-            mirron_end=time.time()
-            print("mirrored stuff took {} seconds".format(mirron_end-start))
-    else:
+    with mirrored_strategy.scope():
         print(mirrored_strategy)
-        print("begin NOT mirrored stuff")
+        print("begin mirrored stuff")
+
+        data_start=time.time()
+        dataset_dict=yvae_get_dataset_train(batch_size=args.batch_size, dataset_names=args.dataset_names, image_dim=args.image_dim,mirrored_strategy=mirrored_strategy)
+        test_dataset_dict=yvae_get_dataset_test(batch_size=args.batch_size, dataset_names=args.dataset_names, image_dim=args.image_dim, mirrored_strategy=mirrored_strategy)
+        data_end=time.time()
+        print("seeting up data took {} seconds ".format(data_end-data_start))
         
+        optimizer_start=time.time()
         optimizer=keras.optimizers.Adam(learning_rate=args.init_lr)
         unfrozen_optimizer=keras.optimizers.Adam(learning_rate=0.00001)
-        print("optimizers took {} seconds".format(time.time()-start))
+        print("optimizers took {} seconds".format(time.time()-optimizer_start))
         #optimizer=tf.keras.mixed_precision.LossScaleOptimizer(optimizer)
+        process = psutil.Process(os.getpid())
+        pct=process.memory_percent()
+        print('mem pct',pct)
+        time.sleep(5)
+        gc.collect()
         if args.load:
             print("loading from saved model")
             shared_partial=tf.keras.models.load_model(save_model_folder+SHARED_ENCODER_NAME)
@@ -129,21 +91,20 @@ def objective_unit(trial,args):
         else:
             print("not loading from saved")
             encoder_start=time.time()
-            encoder=get_encoder(input_shape,args.latent_dim, use_residual=args.use_residual)
+            print('encoder args',input_shape,args.latent_dim, args.use_residual)
+            if len(args.pretrained_creativity_path)==0:
+                encoder=get_encoder(input_shape,args.latent_dim, use_residual=args.use_residual, use_bn=args.use_bn,use_gn=args.use_gn)
+            else:
+                encoder=tf.keras.models.load_model(args.pretrained_creativity_path)
+                print('loaded from creativity!')
             encoder_end=time.time()
             print('getting encoder took {} time'.format(encoder_end-encoder_start))
             mid_name=ENCODER_CONV_NAME.format(2)
-            unit_list=get_unit_list(input_shape,args.latent_dim,n_classes,encoder,mid_name=mid_name, use_residual=args.use_residual)
+            unit_list=get_unit_list(input_shape,args.latent_dim,n_classes,encoder,mid_name=mid_name, use_residual=args.use_residual,use_bn=args.use_bn)
             print("unit_list time took {}".format(time.time()-encoder_end))
 
-        mirrored_strategy=None
-
-        data_start=time.time()
-        print("mirrored stuff minus data took {} seconds".format(data_start-start))
-        dataset_dict=yvae_get_dataset_train(batch_size=args.batch_size, dataset_names=args.dataset_names, image_dim=args.image_dim,mirrored_strategy=mirrored_strategy)
-        test_dataset_dict=yvae_get_dataset_test(batch_size=args.batch_size, dataset_names=args.dataset_names, image_dim=args.image_dim, mirrored_strategy=mirrored_strategy)
-        data_end=time.time()
-        print("seeting up data took {} seconds ".format(data_end-data_start))
+        mirron_end=time.time()
+        print("mirrored stuff took {} seconds".format(mirron_end-start))
     trainer=VAE_Unit_Trainer(unit_list, args.epochs, dataset_dict=dataset_dict, test_dataset_dict=test_dataset_dict, 
                              optimizer=optimizer, log_dir=log_dir, mirrored_strategy=mirrored_strategy, kl_loss_scale=args.kl_loss_scale,start_epoch=start_epoch,
                              unfreezing_epoch=args.unfreezing_epoch, fine_tuning=args.fine_tuning, unfrozen_optimizer=unfrozen_optimizer )
