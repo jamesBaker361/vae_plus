2c8eb3348eabb9644cb5bf1fce0de611e5bb2209
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-07-03 16:45:43 -0400

    cleaned up data_utils

--- data_utils/ihatejupyter.py
+++
@@ -1,6 +0,0 @@
-from processing_utils import *
-
-path_list=["jlbaker361/flickr_humans_10k","jlbaker361/anime_faces_10k"]
-dataset=get_labeled_datasets_generator_train(image_dim=64,path_list=path_list)
-
-print(next(iter(dataset)))
